a801276c8ca3766997d6269939a420a6000c5153
nikbratusa <nikbratusa@gmail.com>
2020-08-23 12:26:13 +0200

    Changed app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Hi hehehsadadadae.sdasdada bal bal adsadada
+          Smo ze skor na polovici
         </p>
         <a
           className="App-link"
